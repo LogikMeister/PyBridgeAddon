c1357a7494ebc65166c5ec0fba717277032365d9
LogikMeister <qbshuan@qq.com>
2023-07-07 22:08:38 +0800

    0.0.1

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import terser from "@rollup/plugin-terser"
 import typescript from "@rollup/plugin-typescript"
 import resolve from "@rollup/plugin-node-resolve"
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
     input: "src/ts/index.ts",
@@ -24,6 +25,7 @@ export default {
         typescript({
             tsconfig: "./tsconfig.json"
         }),
-        resolve()
+        resolve(),
+        commonjs()
     ]
 }
